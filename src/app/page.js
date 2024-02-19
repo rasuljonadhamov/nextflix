b09d6e4cd20c7201023c28d98bd558720e60885d
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-19 10:47:37 +0500

    created hero component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
 import Banner from "../../components/banner/Banner";
 
 export default function Home() {
-  return <Banner />;
+  return (
+    <div style={{ height: "100vh" }}>
+      <h1>Nextlix</h1>
+      <Banner
+        title="Warning the Red "
+        subTitle="Dedpul brooo"
+        imgUrl="https://picsum.photos/600/600"
+      />
+    </div>
+  );
 }
